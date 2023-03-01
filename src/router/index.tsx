31ee83fb50fc18aa4f2860073b3858b7bf34c774
Andrey <andrew.gorba4ev2018@yandex.ru>
2023-03-01 11:33:41 +0300

    check refresh token

--- src/router/index.tsx
+++ src/router/index.tsx
@@ -1,126 +1,132 @@
-import React, { useEffect, useState } from "react";
-import { Routes, Route, Navigate } from "react-router-dom";
-import { useAppDispatch, useAppSelector } from "../hooks/redux";
-import Content from "../layouts/content/Content";
-import Header from "../layouts/header/Header";
-import Home from "../pages/home/Home";
-import Operation from "../pages/operation/Operation";
-import History from "../pages/history/History";
-import Sidebar from "../layouts/sidebar/Sidebar";
-import Chat from "../components/chat/Chat";
-import useWindowDimensions from "../hooks/useWindowDimensions";
-import cardsIcon from "../assets/images/sidebar/cards.png";
-import historyIcon from "../assets/images/sidebar/history.png";
-import swapIcon from "../assets/images/sidebar/swap.png";
-import classes from "./Route.module.css";
-import Card from "../pages/card/Card";
-import { checkAuth, getTransactions } from "../store/reducers/ActionCreators";
-import { isNotEmptyObject } from "../utils/isNotEmptyObject";
-import { Spin } from "antd";
+import React, { useEffect, useState } from 'react'
+import { Routes, Route, Navigate } from 'react-router-dom'
+import { useAppDispatch, useAppSelector } from '../hooks/redux'
+import Content from '../layouts/content/Content'
+import Header from '../layouts/header/Header'
+import Home from '../pages/home/Home'
+import Operation from '../pages/operation/Operation'
+import History from '../pages/history/History'
+import Sidebar from '../layouts/sidebar/Sidebar'
+import Chat from '../components/chat/Chat'
+import useWindowDimensions from '../hooks/useWindowDimensions'
+import cardsIcon from '../assets/images/sidebar/cards.png'
+import historyIcon from '../assets/images/sidebar/history.png'
+import swapIcon from '../assets/images/sidebar/swap.png'
+import classes from './Route.module.css'
+import Card from '../pages/card/Card'
+import { checkAuth, getTransactions } from '../store/reducers/ActionCreators'
+import { isNotEmptyObject } from '../utils/isNotEmptyObject'
+import { Spin } from 'antd'
 
 const tabs = [
-  {
-    name: "Обмен валют",
-    url: "/home",
-    icon: swapIcon,
-    page: <Home />,
-  },
-  {
-    name: "Мои карты",
-    url: "/cards",
-    icon: cardsIcon,
-    page: <Card />,
-  },
-  {
-    name: "История операций",
-    url: "/history",
-    icon: historyIcon,
-    page: <History />,
-  },
-];
+	{
+		name: 'Обмен валют',
+		url: '/home',
+		icon: swapIcon,
+		page: <Home />,
+	},
+	{
+		name: 'Мои карты',
+		url: '/cards',
+		icon: cardsIcon,
+		page: <Card />,
+	},
+	{
+		name: 'История операций',
+		url: '/history',
+		icon: historyIcon,
+		page: <History />,
+	},
+]
 const RouterApp = () => {
-  const { auth, user, isLoading, error } = useAppSelector(
-    (state) => state.authReducer
-  );
-  const [isExpand, setIsExpand] = useState(true);
-  const { width } = useWindowDimensions();
-  const isWideWidth = width > 1400;
-  const isFullSideBar = isWideWidth && isExpand;
-  const dispatch = useAppDispatch();
+	const { auth, user, isLoading, error } = useAppSelector(
+		state => state.authReducer
+	)
+	const [isExpand, setIsExpand] = useState(true)
+	const [isCheckAuth, setIsCheckAuth] = useState(false)
+	const { width } = useWindowDimensions()
+	const isWideWidth = width > 1400
+	const isFullSideBar = isWideWidth && isExpand
+	const dispatch = useAppDispatch()
 
-  useEffect(() => {
-    dispatch(getTransactions());
-    const interval = setInterval(() => {
-      dispatch(getTransactions());
-    }, 10000);
+	const refreshToken = localStorage.getItem('refreshToken')
+	useEffect(() => {
+		dispatch(getTransactions())
+		const interval = setInterval(() => {
+			dispatch(getTransactions())
+		}, 10000)
 
-    return () => clearInterval(interval);
-  }, []);
+		return () => clearInterval(interval)
+	}, [])
 
-  useEffect(() => {
-    console.log("checkAuth", checkAuth);
-    dispatch(checkAuth());
-  }, [auth]);
+	useEffect(() => {
+		console.log('checkAuth', checkAuth)
+		setIsCheckAuth(prev => !prev)
+		dispatch(checkAuth())
+	}, [auth])
 
-  useEffect(() => {
-    if (isWideWidth) {
-      setIsExpand(true);
-    } else {
-      setIsExpand(false);
-    }
-  }, [isWideWidth]);
+	useEffect(() => {
+		if (isWideWidth) {
+			setIsExpand(true)
+		} else {
+			setIsExpand(false)
+		}
+	}, [isWideWidth])
 
-  return (
-    <div>
-      <Header />
-      <Chat />
-      <div className={classes.container}>
-        {isLoading ? (
-          <div
-            className={classes.spinContainer}
-            style={{
-              display: "flex",
-              alignItems: "center",
-              justifyContent: "center",
-              height: "100vh",
-              width: "100%",
-            }}
-          >
-            <Spin size="large" />
-          </div>
-        ) : (
-          <>
-            {isNotEmptyObject(user) && (
-              <Sidebar
-                user={user}
-                tabs={tabs}
-                isFullSideBar={isFullSideBar}
-                isExpand={isExpand}
-                isWideWidth={isWideWidth}
-                setExpand={setIsExpand}
-              />
-            )}
-            <Content>
-              <Routes>
-                {isNotEmptyObject(user) ? (
-                  <>
-                    {tabs.map((item, key) => (
-                      <Route key={key} path={item.url} element={item.page} />
-                    ))}
-                    <Route path="/operation/:id" element={<Operation />} />
-                  </>
-                ) : (
-                  <Route path="/home" element={<Home />} />
-                )}
+	console.log('user', user)
+	console.log('auth', auth)
+	console.log('isLoading', isLoading)
+	return (
+		<div>
+			<Header />
+			<Chat />
+			<div className={classes.container}>
+				{isLoading ? (
+					<div
+						className={classes.spinContainer}
+						style={{
+							display: 'flex',
+							alignItems: 'center',
+							justifyContent: 'center',
+							height: '100vh',
+							width: '100%',
+						}}
+					>
+						<Spin size="large" />
+					</div>
+				) : (
+					<>
+						{isNotEmptyObject(user) && (
+							<Sidebar
+								user={user}
+								tabs={tabs}
+								isFullSideBar={isFullSideBar}
+								isExpand={isExpand}
+								isWideWidth={isWideWidth}
+								setExpand={setIsExpand}
+							/>
+						)}
+						<Content>
+							<Routes>
+								{refreshToken ? (
+									<>
+										{tabs.map((item, key) => (
+											<Route key={key} path={item.url} element={item.page} />
+										))}
+										<Route path="/operation/:id" element={<Operation />} />
+									</>
+								) : (
+									<Route path="/home" element={<Home />} />
+								)}
 
-                <Route path="*" element={<Navigate to="/home" replace />} />
-              </Routes>
-            </Content>
-          </>
-        )}
-      </div>
-    </div>
-  );
-};
+								<Route path="*" element={<Navigate to="/home" replace />} />
+							</Routes>
+						</Content>
+					</>
+				)}
+			</div>
+		</div>
+	)
+}
 
-export default RouterApp;
+export default RouterApp
